b629a3bcfa482eeb8261de8e6b725aff5b3f6ad6
Link1J <jrairwin@sympatico.ca>
2022-11-19 16:40:00 -0500

    Fixup winrt_task

--- include/ljh/coroutine/winrt_task.hpp
+++ include/ljh/coroutine/winrt_task.hpp
@@ -12,76 +12,6 @@
 namespace ljh::co
 {
 	template<typename TResult>
-	struct winrt_task;
-} // namespace winrt
-
-namespace ljh::_co
-{
-	template<typename TResult>
-	struct winrt_task_completed_handler;
-
-	template<typename D, typename TResult>
-	struct consume_task
-	{
-		auto               Completed(winrt_task_completed_handler<TResult> const& handler) const;
-		[[nodiscard]] auto Completed() const;
-		auto               GetResults() const;
-		auto               get() const;
-		auto               wait_for(winrt::Windows::Foundation::TimeSpan const& timeout) const;
-	};
-} // namespace winrt
-
-namespace winrt::impl
-{
-	template<typename TResult>
-	struct category<ljh::co::winrt_task<TResult>>
-	{
-		using type = interface_category;
-	};
-	template<typename TResult>
-	inline constexpr auto& name_v<ljh::co::winrt_task<TResult>> = L"winrt_task";
-	template<typename TResult>
-	inline constexpr guid guid_v<ljh::co::winrt_task<TResult>>{"7221256A-FB66-44FB-8526-D07565FF6102"};
-	template<typename TResult>
-	struct abi<ljh::co::winrt_task<TResult>>
-	{
-		struct __declspec(novtable) type : inspectable_abi
-		{
-			virtual int32_t __stdcall put_Completed(void*) noexcept         = 0;
-			virtual int32_t __stdcall get_Completed(void**) noexcept        = 0;
-			virtual int32_t __stdcall GetResults(arg_out<TResult>) noexcept = 0;
-		};
-	};
-	template<typename TResult>
-	struct consume<ljh::co::winrt_task<TResult>>
-	{
-		template<typename D>
-		using type = consume_task<D, TResult>;
-	};
-
-	template<typename TResult>
-	struct category<ljh::_co::winrt_task_completed_handler<TResult>>
-	{
-		using type = interface_category;
-	};
-	template<typename TResult>
-	inline constexpr auto& name_v<ljh::_co::winrt_task_completed_handler<TResult>> = L"winrt_task_completed_handler";
-	template<typename TResult>
-	inline constexpr guid guid_v<ljh::_co::winrt_task_completed_handler<TResult>>{"3C736D26-5C53-46D7-AF11-14F91C834852"};
-	template<typename TResult>
-	struct abi<ljh::_co::winrt_task_completed_handler<TResult>>
-	{
-		struct __declspec(novtable) type : unknown_abi
-		{
-			virtual int32_t __stdcall Invoke(void*, int32_t) noexcept = 0;
-		};
-	};
-
-} // namespace winrt::impl
-
-namespace ljh::co
-{
-	template<typename TResult>
 	struct winrt_task : Windows::Foundation::IInspectable, impl::consume_t<winrt_task<TResult>>, impl::require<winrt_task<TResult>, winrt::Windows::Foundation::IAsyncInfo>
 	{
 		winrt_task(std::nullptr_t = nullptr) noexcept
@@ -90,9 +20,15 @@ namespace ljh::co
 			: winrt::Windows::Foundation::IInspectable(ptr, take_ownership_from_abi)
 		{}
 	};
+
+	template<typename TResult>
+	inline winrt::impl::await_adapter<winrt_task<TResult>> operator co_await(winrt_task<TResult> const& async)
+	{
+		return { async };
+	}
 }
 
-namespace ljh::_co
+namespace ljh::co::__
 {
 	template<typename TResult>
 	struct winrt_task_completed_handler : winrt::Windows::Foundation::IUnknown
@@ -102,168 +38,190 @@ namespace ljh::_co
 		winrt_task_completed_handler(void* ptr, take_ownership_from_abi_t) noexcept
 			: winrt::Windows::Foundation::IUnknown(ptr, take_ownership_from_abi)
 		{}
+
 		template<typename L>
-		winrt_task_completed_handler(L lambda);
+		winrt_task_completed_handler(L lambda)
+			: winrt_task_completed_handler(winrt::impl::make_delegate<winrt_task_completed_handler<TResult>>(std::forward<L>(lambda)))
+		{}
 		template<typename F>
-		winrt_task_completed_handler(F* function);
+		winrt_task_completed_handler(F* function)
+			: winrt_task_completed_handler([=](auto&&... args) { return function(args...); })
+		{}
 		template<typename O, typename M>
-		winrt_task_completed_handler(O* object, M method);
+		winrt_task_completed_handler(O* object, M method)
+			: winrt_task_completed_handler([=](auto&&... args) { return ((*object).*(method))(args...); })
+		{}
 		template<typename O, typename M>
-		winrt_task_completed_handler(winrt::com_ptr<O>&& object, M method);
+		winrt_task_completed_handler(winrt::com_ptr<O>&& object, M method)
+			: winrt_task_completed_handler([o = std::move(object), method](auto&&... args) { return ((*o).*(method))(args...); })
+		{}
 		template<typename O, typename M>
-		winrt_task_completed_handler(winrt::weak_ref<O>&& object, M method);
-		auto operator()(ljh::co::winrt_task<TResult> const& asyncInfo, winrt::Windows::Foundation::AsyncStatus const& asyncStatus) const;
-	};
-
-	template<typename D, typename TResult>
-	auto consume_task<D, TResult>::Completed(winrt_task_completed_handler<TResult> const& handler) const
-	{
-		check_hresult(WINRT_IMPL_SHIM(winrt_task<TResult>)->put_Completed(*(void**)(&handler)));
-	}
-	template<typename D, typename TResult>
-	auto consume_task<D, TResult>::Completed() const
-	{
-		void* winrt_impl_result{};
-		check_hresult(WINRT_IMPL_SHIM(winrt_task<TResult>)->get_Completed(&winrt_impl_result));
-		return winrt_task_completed_handler<TResult>{winrt_impl_result, take_ownership_from_abi};
-	}
-	template<typename D, typename TResult>
-	auto consume_task<D, TResult>::GetResults() const
-	{
-		TResult winrt_impl_result{empty_value<TResult>()};
-		check_hresult(WINRT_IMPL_SHIM(winrt_task<TResult>)->GetResults(put_abi(winrt_impl_result)));
-		return winrt_impl_result;
-	}
-	template<typename D, typename TResult>
-	auto consume_task<D, TResult>::get() const
-	{
-		return impl::wait_get(static_cast<task<TResult> const&>(static_cast<D const&>(*this)));
-	}
-	template<typename D, typename TResult>
-	auto consume_task<D, TResult>::wait_for(winrt::Windows::Foundation::TimeSpan const& timeout) const
-	{
-		return impl::wait_for(static_cast<winrt_task<TResult> const&>(static_cast<D const&>(*this)), timeout);
-	}
-}
-
-namespace winrt::impl
-{
-	template<typename D, typename TResult>
-	struct produce<D, ljh::co::winrt_task<TResult>> : produce_base<D, ljh::co::winrt_task<TResult>>
-	{
-		int32_t __stdcall put_Completed(void* handler) noexcept final
-		try
-		{
-			typename D::abi_guard guard(this->shim());
-			this->shim().Completed(*reinterpret_cast<ljh::_co::winrt_task_completed_handler<TResult> const*>(&handler));
-			return 0;
-		}
-		catch (...)
-		{
-			return to_hresult();
-		}
-		int32_t __stdcall get_Completed(void** winrt_impl_result) noexcept final
-		try
-		{
-			clear_abi(winrt_impl_result);
-			typename D::abi_guard guard(this->shim());
-			*winrt_impl_result = detach_from<ljh::_co::winrt_task_completed_handler<TResult>>(this->shim().Completed());
-			return 0;
-		}
-		catch (...)
-		{
-			return to_hresult();
-		}
-		int32_t __stdcall GetResults(arg_out<TResult> winrt_impl_result) noexcept final
-		try
-		{
-			clear_abi(winrt_impl_result);
-			typename D::abi_guard guard(this->shim());
-			*winrt_impl_result = detach_from<TResult>(this->shim().GetResults());
-			return 0;
-		}
-		catch (...)
-		{
-			return to_hresult();
-		}
-	};
-
-	template<typename H, typename TResult>
-	struct delegate<ljh::_co::winrt_task_completed_handler<TResult>, H> final : implements_delegate<ljh::_co::winrt_task_completed_handler<TResult>, H>
-	{
-		delegate(H&& handler)
-			: implements_delegate<ljh::_co::winrt_task_completed_handler<TResult>, H>(std::forward<H>(handler))
+		winrt_task_completed_handler(winrt::weak_ref<O>&& object, M method)
+			: winrt_task_completed_handler([o = std::move(object), method](auto&&... args) { if (auto s = o.get()) { ((*s).*(method))(args...); }})
 		{}
 
-		int32_t __stdcall Invoke(void* asyncInfo, int32_t asyncStatus) noexcept final
-		try
+		auto operator()(winrt_task<TResult> const& asyncInfo, winrt::Windows::Foundation::AsyncStatus const& asyncStatus) const
 		{
-			(*this)(*reinterpret_cast<ljh::co::winrt_task<TResult> const*>(&asyncInfo), *reinterpret_cast<winrt::Windows::Foundation::AsyncStatus const*>(&asyncStatus));
-			return 0;
+			check_hresult((*(winrt::impl::abi_t<winrt_task_completed_handler<TResult>>**)this)->Invoke(*(void**)(&asyncInfo), static_cast<int32_t>(asyncStatus)));
 		}
-		catch (...)
+	};
+
+	template<typename D, typename TResult>
+	struct consume_task
+	{
+		template<typename T>
+		decltype(auto) cast_self() noexcept const
 		{
-			return to_hresult();
+			return static_cast<T const&>(static_cast<D const&>(*this));
+		}
+
+		template<typename T>
+		decltype(auto) shim() noexcept const
+		{
+			return (*(winrt::impl::abi_t<T>**)&this->cast_self<D, T>());
+		}
+
+		auto Completed(winrt_task_completed_handler<TResult> const& handler) const
+		{
+			winrt::check_hresult(shim<winrt_task<TResult>>()->put_Completed(*(void**)(&handler)));
+		}
+
+		[[nodiscard]] auto Completed() const
+		{
+			void* winrt_impl_result{};
+			winrt::check_hresult(shim<winrt_task<TResult>>()->get_Completed(&winrt_impl_result));
+			return winrt_task_completed_handler<TResult>{winrt_impl_result, take_ownership_from_abi};
+		}
+
+		auto GetResults() const
+		{
+			TResult winrt_impl_result{ winrt::impl::empty_value<TResult>() };
+			winrt::check_hresult(shim<winrt_task<TResult>>()->GetResults(put_abi(winrt_impl_result)));
+			return winrt_impl_result;
+		}
+
+		auto get() const
+		{
+			return winrt::impl::wait_get(cast_self<winrt_task<TResult>>());
+		}
+
+		auto wait_for(winrt::Windows::Foundation::TimeSpan const& timeout) const
+		{
+			return winrt::impl::wait_for(cast_self<winrt_task<TResult>>(), timeout);
 		}
 	};
 }
 
-namespace ljh::_co
+template<typename TResult>
+struct winrt::impl::category<ljh::co::winrt_task<TResult>>
 {
-	template<typename TResult>
-	template<typename L>
-	winrt_task_completed_handler<TResult>::winrt_task_completed_handler(L handler)
-		: winrt_task_completed_handler(impl::make_delegate<winrt_task_completed_handler<TResult>>(std::forward<L>(handler)))
-	{}
-	template<typename TResult>
-	template<typename F>
-	winrt_task_completed_handler<TResult>::winrt_task_completed_handler(F* handler)
-		: winrt_task_completed_handler([=](auto&&... args) { return handler(args...); })
-	{}
-	template<typename TResult>
-	template<typename O, typename M>
-	winrt_task_completed_handler<TResult>::winrt_task_completed_handler(O* object, M method)
-		: winrt_task_completed_handler([=](auto&&... args) { return ((*object).*(method))(args...); })
-	{}
-	template<typename TResult>
-	template<typename O, typename M>
-	winrt_task_completed_handler<TResult>::winrt_task_completed_handler(winrt::com_ptr<O>&& object, M method)
-		: winrt_task_completed_handler([o = std::move(object), method](auto&&... args) { return ((*o).*(method))(args...); })
-	{}
-	template<typename TResult>
-	template<typename O, typename M>
-	winrt_task_completed_handler<TResult>::winrt_task_completed_handler(winrt::weak_ref<O>&& object, M method)
-		: winrt_task_completed_handler([o = std::move(object), method](auto&&... args) {
-			if (auto s = o.get())
-			{
-				((*s).*(method))(args...);
-			}
-		})
-	{}
-	template<typename TResult>
-	auto winrt_task_completed_handler<TResult>::operator()(ljh::co::winrt_task<TResult> const& asyncInfo, winrt::Windows::Foundation::AsyncStatus const& asyncStatus) const
-	{
-		check_hresult((*(impl::abi_t<task_completed_handler<TResult>>**)this)->Invoke(*(void**)(&asyncInfo), static_cast<int32_t>(asyncStatus)));
-	}
-} // namespace winrt
-
-namespace ljh::co
+	using type = interface_category;
+};
+template<typename TResult>
+inline constexpr auto& winrt::impl::name_v<ljh::co::winrt_task<TResult>> = L"winrt_task";
+template<typename TResult>
+inline constexpr winrt::guid winrt::impl::guid_v<ljh::co::winrt_task<TResult>>{"7221256A-FB66-44FB-8526-D07565FF6102"};
+template<typename TResult>
+struct winrt::impl::abi<ljh::co::winrt_task<TResult>>
 {
-	template<typename TResult>
-	inline winrt::impl::await_adapter<ljh::co::winrt_task<TResult>> operator co_await(ljh::co::winrt_task<TResult> const& async)
+	struct __declspec(novtable) type : inspectable_abi
 	{
-		return {async};
-	}
-}
-
-namespace winrt::impl
-{
-	template<typename TResult>
-	struct async_completed_handler<ljh::co::winrt_task<TResult>>
-	{
-		using type = ljh::_co::task_completed_handler<TResult>;
+		virtual int32_t __stdcall put_Completed(void*) noexcept = 0;
+		virtual int32_t __stdcall get_Completed(void**) noexcept = 0;
+		virtual int32_t __stdcall GetResults(arg_out<TResult>) noexcept = 0;
 	};
-}
+};
+template<typename TResult>
+struct winrt::impl::consume<ljh::co::winrt_task<TResult>>
+{
+	template<typename D>
+	using type = ljh::co::__::consume_task<D, TResult>;
+};
+template<typename TResult>
+struct winrt::impl::async_completed_handler<ljh::co::winrt_task<TResult>>
+{
+	using type = ljh::co::__::winrt_task_completed_handler<TResult>;
+};
+
+template<typename TResult>
+struct winrt::impl::category<ljh::co::__::winrt_task_completed_handler<TResult>>
+{
+	using type = interface_category;
+};
+template<typename TResult>
+inline constexpr auto& winrt::impl::name_v<ljh::co::__::winrt_task_completed_handler<TResult>> = L"winrt_task_completed_handler";
+template<typename TResult>
+inline constexpr winrt::guid winrt::impl::guid_v<ljh::co::__::winrt_task_completed_handler<TResult>>{"3C736D26-5C53-46D7-AF11-14F91C834852"};
+template<typename TResult>
+struct winrt::impl::abi<ljh::co::__::winrt_task_completed_handler<TResult>>
+{
+	struct __declspec(novtable) type : unknown_abi
+	{
+		virtual int32_t __stdcall Invoke(void*, int32_t) noexcept = 0;
+	};
+};
+
+template<typename D, typename TResult>
+struct winrt::impl::produce<D, ljh::co::winrt_task<TResult>> : winrt::impl::produce_base<D, ljh::co::winrt_task<TResult>>
+{
+	int32_t __stdcall put_Completed(void* handler) noexcept final
+	try
+	{
+		typename D::abi_guard guard(this->shim());
+		this->shim().Completed(*reinterpret_cast<ljh::co::__::winrt_task_completed_handler<TResult> const*>(&handler));
+		return 0;
+	}
+	catch (...)
+	{
+		return to_hresult();
+	}
+
+	int32_t __stdcall get_Completed(void** winrt_impl_result) noexcept final
+	try
+	{
+		clear_abi(winrt_impl_result);
+		typename D::abi_guard guard(this->shim());
+		*winrt_impl_result = detach_from<ljh::co::__::winrt_task_completed_handler<TResult>>(this->shim().Completed());
+		return 0;
+	}
+	catch (...)
+	{
+		return to_hresult();
+	}
+
+	int32_t __stdcall GetResults(arg_out<TResult> winrt_impl_result) noexcept final
+	try
+	{
+		clear_abi(winrt_impl_result);
+		typename D::abi_guard guard(this->shim());
+		*winrt_impl_result = detach_from<TResult>(this->shim().GetResults());
+		return 0;
+	}
+	catch (...)
+	{
+		return to_hresult();
+	}
+};
+
+template<typename H, typename TResult>
+struct winrt::impl::delegate<ljh::co::__::winrt_task_completed_handler<TResult>, H> final 
+	: winrt::impl::implements_delegate<ljh::co::__::winrt_task_completed_handler<TResult>, H>
+{
+	delegate(H&& handler)
+		: implements_delegate<ljh::co::__::winrt_task_completed_handler<TResult>, H>(std::forward<H>(handler))
+	{}
+
+	int32_t __stdcall Invoke(void* asyncInfo, int32_t asyncStatus) noexcept final
+	try
+	{
+		(*this)(*reinterpret_cast<ljh::co::winrt_task<TResult> const*>(&asyncInfo), *reinterpret_cast<winrt::Windows::Foundation::AsyncStatus const*>(&asyncStatus));
+		return 0;
+	}
+	catch (...)
+	{
+		return to_hresult();
+	}
+};
 
 template<typename TResult, typename... Args>
 struct std::coroutine_traits<ljh::co::winrt_task<TResult>, Args...>
@@ -290,6 +248,6 @@ struct std::coroutine_traits<ljh::co::winrt_task<TResult>, Args...>
 			m_result = value;
 		}
 
-		TResult m_result{winrt::impl::empty_value<TResult>()};
+		TResult m_result{ winrt::impl::empty_value<TResult>() };
 	};
 };
