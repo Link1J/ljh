4555a7dceb2b619c560f1aba0728eb7c0ceec41e
Jared Irwin <jrairwin@sympatico.ca>
2024-02-19 14:46:08 -0500

    Add .clang-format and start formating

--- include/ljh/ranges/views.hpp
+++ include/ljh/ranges/views.hpp
@@ -6,9 +6,9 @@
 
 // views.hpp - v1.0
 // SPDX-License-Identifier: BSL-1.0
-// 
+//
 // Requires C++20
-// 
+//
 // ABOUT
 //
 // USAGE
@@ -22,85 +22,79 @@
 
 namespace ljh
 {
-	template<typename T>
-	inline constexpr auto constructor = []() consteval 
-	{
-		return []<typename... Args>(Args&&... args) requires std::constructible_from<T, Args...>
-		{
-			return T(std::forward<Args>(args)...);
-		}; 
-	}();
+    template<typename T>
+    inline constexpr auto constructor = []() consteval {
+        return []<typename... Args>(Args&&... args)
+            requires std::constructible_from<T, Args...>
+        { return T(std::forward<Args>(args)...); };
+    }();
 }
 
 #if __has_include(<ranges>) && __cpp_lib_ranges
 namespace ljh::ranges::views
 {
-	template<typename T>
-	inline constexpr auto transform_into = std::ranges::views::transform(constructor<T>);
+    template<typename T>
+    inline constexpr auto transform_into = std::ranges::views::transform(constructor<T>);
 
-	inline constexpr adaptor transform_tuple = []<std::ranges::viewable_range R, typename F>(R&& r, F&& pred) 
-	{
-		return std::ranges::views::transform(std::forward<R>(r), [pred = std::forward<F>(pred)]<typename T>(T&& t) 
-		{
-			return std::apply(pred, std::forward<T>(t));
-		});
-	};
+    inline constexpr adaptor transform_tuple = []<std::ranges::viewable_range R, typename F>(R&& r, F&& pred) {
+        return std::ranges::views::transform(std::forward<R>(r),
+                                             [pred = std::forward<F>(pred)]<typename T>(T&& t) { return std::apply(pred, std::forward<T>(t)); });
+    };
 
 #if __cpp_lib_ranges_zip >= 202110L
-	namespace __zip
-	{
-		template<std::ranges::viewable_range... R>
-		struct closure : range_adaptor_closure<closure<R...>>
-		{
-			std::tuple<R...> range;
+    namespace __zip
+    {
+        template<std::ranges::viewable_range... R>
+        struct closure : range_adaptor_closure<closure<R...>>
+        {
+            std::tuple<R...> range;
 
-			closure(R&&... ranges)
-				: range(ranges...)
-			{}
+            closure(R&&... ranges)
+                : range(ranges...)
+            {}
 
-			template<std::ranges::viewable_range R2>
-			constexpr auto operator()(R2&& r) &
-			{
-				return std::apply([&r]<typename... R3>(R3&&... o) { return std::ranges::zip_view(std::forward<R2>(r), std::forward<R3>(o)...); }, range);
-			}
+            template<std::ranges::viewable_range R2>
+            constexpr auto operator()(R2&& r) &
+            {
+                return std::apply([&r]<typename... R3>(R3&&... o) { return std::ranges::zip_view(std::forward<R2>(r), std::forward<R3>(o)...); }, range);
+            }
 
-			template<std::ranges::viewable_range R2>
-			constexpr auto operator()(R2&& r) const&
-			{
-				return std::apply([&r]<typename... R3>(R3&&... o) { return std::ranges::zip_view(std::forward<R2>(r), std::forward<R3>(o)...); }, range);
-			}
+            template<std::ranges::viewable_range R2>
+            constexpr auto operator()(R2&& r) const&
+            {
+                return std::apply([&r]<typename... R3>(R3&&... o) { return std::ranges::zip_view(std::forward<R2>(r), std::forward<R3>(o)...); }, range);
+            }
 
-			template<std::ranges::viewable_range R2>
-			constexpr auto operator()(R2&& r) &&
-			{
-				return std::apply([&r]<typename... R3>(R3&&... o) { return std::ranges::zip_view(std::forward<R2>(r), std::forward<R3>(o)...); }, range);
-			}
+            template<std::ranges::viewable_range R2>
+            constexpr auto operator()(R2&& r) &&
+            {
+                return std::apply([&r]<typename... R3>(R3&&... o) { return std::ranges::zip_view(std::forward<R2>(r), std::forward<R3>(o)...); }, range);
+            }
 
-			template<std::ranges::viewable_range R2>
-			constexpr auto operator()(R2&& r) const&&
-			{
-				return std::apply([&r]<typename... R3>(R3&&... o) { return std::ranges::zip_view(std::forward<R2>(r), std::forward<R3>(o)...); }, range);
-			}
-		};
+            template<std::ranges::viewable_range R2>
+            constexpr auto operator()(R2&& r) const&&
+            {
+                return std::apply([&r]<typename... R3>(R3&&... o) { return std::ranges::zip_view(std::forward<R2>(r), std::forward<R3>(o)...); }, range);
+            }
+        };
 
-		struct fn
-		{
-			template<std::ranges::viewable_range... R>
-			constexpr auto operator()(R&&... r) const noexcept
-			{
-				return closure(std::forward<R>(r)...);
-			}
-		};
-	}
+        struct fn
+        {
+            template<std::ranges::viewable_range... R>
+            constexpr auto operator()(R&&... r) const noexcept
+            {
+                return closure(std::forward<R>(r)...);
+            }
+        };
+    } // namespace __zip
 
-	inline constexpr auto zip = __zip::fn{};
+    inline constexpr auto zip = __zip::fn{};
 #endif
 
 #if __cpp_lib_ranges_fold >= 202207L
-	inline constexpr adaptor fold_left = []<std::ranges::input_range R, typename T, typename F>(R&& r, T i, F f) 
-	{
-		return std::ranges::fold_left(std::forward<R>(r), i, f);
-	};
+    inline constexpr adaptor fold_left = []<std::ranges::input_range R, typename T, typename F>(R&& r, T i, F f) {
+        return std::ranges::fold_left(std::forward<R>(r), i, f);
+    };
 #endif
-}
+} // namespace ljh::ranges::views
 #endif
