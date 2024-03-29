497099825084324dc0f340b904a6d4093dce69b2
Link1J <jrairwin@sympatico.ca>
2024-03-29 15:28:01 -0400

    Add defer

---
+++ include/ljh/defer.hpp
@@ -0,0 +1,223 @@
+
+//          Copyright Jared Irwin 2024-2024
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+// defer.hpp - v1.0
+// SPDX-License-Identifier: BSL-1.0
+//
+// Requires C++11
+//
+// ABOUT
+//
+// USAGE
+//
+// Version History
+//     1.0 Inital Version
+
+#pragma once
+#include "cpp_version.hpp"
+#include <utility>
+#include <exception>
+
+namespace ljh
+{
+    namespace __
+    {
+        template<typename D, typename T>
+        struct defer_base
+        {
+            static constexpr bool _nothrow_constructible = std::is_nothrow_constructible<T, T>::value;
+
+            ~defer_base() noexcept(noexcept(f()))
+            {
+                if (static_cast<D&>(*this)._run())
+                    f();
+            }
+
+            defer_base(defer_base const&)            = delete;
+            defer_base& operator=(defer_base const&) = delete;
+            defer_base(defer_base&&)                 = default;
+            defer_base& operator=(defer_base&&)      = default;
+
+        protected:
+            defer_base(T&& f) noexcept(_nothrow_constructible)
+                : f(std::forward<T>(f))
+            {}
+
+        private:
+            T f;
+        };
+
+        template<typename D, typename T>
+        struct condition_defer_base : defer_base<D, T>
+        {
+            condition_defer_base(condition_defer_base&&)            = default;
+            condition_defer_base& operator=(condition_defer_base&&) = default;
+
+            void make_active() noexcept
+            {
+                run = true;
+            }
+
+            void make_inactive() noexcept
+            {
+                run = false;
+            }
+
+            void toggle() noexcept
+            {
+                run = !run;
+            }
+
+        protected:
+            using defer = __::defer_base<D, T>;
+
+            condition_defer_base(bool default_state, T&& f) noexcept(defer::_nothrow_constructible)
+                : defer(std::forward<T>(f))
+                , run(default_state)
+            {}
+
+            bool run;
+        };
+
+        inline bool in_exception() noexcept
+        {
+#if __cpp_lib_uncaught_exceptions >= 201411L
+            return std::uncaught_exceptions() > 0;
+#else
+            return std::uncaught_exception();
+#endif
+        }
+    } // namespace __
+} // namespace ljh
+
+namespace ljh
+{
+    template<typename T>
+    struct scope_exit : __::defer_base<scope_exit<T>, T>
+    {
+        using _base = __::defer_base<scope_exit<T>, T>;
+
+        scope_exit(T&& f) noexcept(_base::_nothrow_constructible)
+            : _base(std::forward<T>(f))
+        {}
+
+        static constexpr bool _run() noexcept
+        {
+            return true;
+        }
+    };
+
+    template<typename T>
+    struct scope_fail : __::defer_base<scope_fail<T>, T>
+    {
+        using _base = __::defer_base<scope_fail<T>, T>;
+
+        scope_fail(T&& f) noexcept(_base::_nothrow_constructible)
+            : _base(std::forward<T>(f))
+        {}
+
+        static bool _run() noexcept
+        {
+            return __::in_exception();
+        }
+    };
+
+    template<typename T>
+    struct scope_success : __::defer_base<scope_success<T>, T>
+    {
+        using _base = __::defer_base<scope_success<T>, T>;
+
+        scope_success(T&& f) noexcept(_base::_nothrow_constructible)
+            : _base(std::forward<T>(f))
+        {}
+
+        static bool _run() noexcept
+        {
+            return !__::in_exception();
+        }
+    };
+
+    template<typename T>
+    struct condition_scope_exit : __::condition_defer_base<condition_scope_exit<T>, T>
+    {
+        using _base = __::condition_defer_base<condition_scope_exit, T>;
+
+        condition_scope_exit(bool default_state, T&& f) noexcept(_base::defer::_nothrow_constructible)
+            : _base(default_state, std::forward<T>(f))
+        {}
+
+        bool _run() const noexcept
+        {
+            return _base::run;
+        }
+    };
+
+    template<typename T>
+    struct condition_scope_fail : __::condition_defer_base<condition_scope_fail<T>, T>
+    {
+        using _base = __::condition_defer_base<condition_scope_fail, T>;
+
+        condition_scope_fail(bool default_state, T&& f) noexcept(_base::defer::_nothrow_constructible)
+            : _base(default_state, std::forward<T>(f))
+        {}
+
+        bool _run() const noexcept
+        {
+            return _base::run && __::in_exception();
+        }
+    };
+
+    template<typename T>
+    struct condition_scope_success : __::condition_defer_base<condition_scope_success<T>, T>
+    {
+        using _base = __::condition_defer_base<condition_scope_success, T>;
+
+        condition_scope_success(bool default_state, T&& f) noexcept(_base::defer::_nothrow_constructible)
+            : _base(default_state, std::forward<T>(f))
+        {}
+
+        bool _run() const noexcept
+        {
+            return _base::run && !__::in_exception();
+        }
+    };
+
+    template<typename T>
+    scope_exit<T> make_scope_exit(T&& f) noexcept(scope_exit<T>::_nothrow_constructible)
+    {
+        return scope_exit<T>{std::forward<T>(f)};
+    }
+
+    template<typename T>
+    scope_fail<T> make_scope_fail(T&& f) noexcept(scope_fail<T>::_nothrow_constructible)
+    {
+        return scope_fail<T>{std::forward<T>(f)};
+    }
+
+    template<typename T>
+    scope_success<T> make_scope_success(T&& f) noexcept(scope_success<T>::_nothrow_constructible)
+    {
+        return scope_success<T>{std::forward<T>(f)};
+    }
+
+    template<typename T>
+    condition_scope_exit<T> make_condition_scope_exit(bool default_state, T&& f) noexcept(condition_scope_exit<T>::_nothrow_constructible)
+    {
+        return condition_scope_exit<T>{default_state, std::forward<T>(f)};
+    }
+
+    template<typename T>
+    condition_scope_fail<T> make_condition_scope_fail(bool default_state, T&& f) noexcept(condition_scope_fail<T>::_nothrow_constructible)
+    {
+        return condition_scope_fail<T>{default_state, std::forward<T>(f)};
+    }
+
+    template<typename T>
+    condition_scope_success<T> make_condition_scope_success(bool default_state, T&& f) noexcept(condition_scope_success<T>::_nothrow_constructible)
+    {
+        return condition_scope_success<T>{default_state, std::forward<T>(f)};
+    }
+} // namespace ljh
---
+++ tests/defer.11.cpp
@@ -0,0 +1,324 @@
+
+//          Copyright Jared Irwin 2024-2024
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include "ljh/defer.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#if LJH_CPP_VERSION <= LJH_CPP17_VERSION
+template<template<typename> class Scope, typename T>
+Scope<T> make(T&& f) noexcept(std::is_nothrow_constructible<Scope<T>, T>::value)
+{
+    return Scope<T>{std::forward<T>(f)};
+}
+#endif
+
+template<template<typename> class Scope>
+int no_exception(int& count)
+{
+#if LJH_CPP_VERSION >= LJH_CPP17_VERSION
+    Scope _{[&count]() mutable { count++; }};
+#else
+    auto _ = make<Scope>([&count]() mutable { count++; });
+#endif
+    return 1;
+}
+
+template<template<typename> class Scope>
+int exception(int& count)
+{
+#if LJH_CPP_VERSION >= LJH_CPP17_VERSION
+    Scope _{[&count]() mutable { count++; }};
+#else
+    auto _ = make<Scope>([&count]() mutable { count++; });
+#endif
+    throw 1;
+}
+
+#if LJH_CPP_VERSION <= LJH_CPP17_VERSION
+template<template<typename> class Scope, typename D, typename T>
+Scope<T> make_with_default(D default_state, T&& f) noexcept(std::is_nothrow_constructible<Scope<T>, D, T>::value)
+{
+    return Scope<T>{default_state, std::forward<T>(f)};
+}
+#endif
+
+template<template<typename> class Scope, typename T, typename F>
+int no_exception_action(T default_state, int& count, F&& action)
+{
+#if LJH_CPP_VERSION >= LJH_CPP17_VERSION
+    Scope _{default_state, [&count]() mutable { count++; }};
+#else
+    auto _ = make_with_default<Scope>(default_state, [&count]() mutable { count++; });
+#endif
+    action(_);
+    return 1;
+}
+
+template<template<typename> class Scope, typename T, typename F>
+int exception_action(T default_state, int& count, F&& action)
+{
+#if LJH_CPP_VERSION >= LJH_CPP17_VERSION
+    Scope _{default_state, [&count]() mutable { count++; }};
+#else
+    auto _ = make_with_default<Scope>(default_state, [&count]() mutable { count++; });
+#endif
+    action(_);
+    throw 1;
+}
+
+TEST_CASE("scope_exit", "[test_11][defer]")
+{
+    int count = 0;
+    CHECK(count == 0);
+    SECTION("No Exception")
+    {
+        CHECK(no_exception<ljh::scope_exit>(count) == 1);
+        REQUIRE(count == 1);
+    }
+    SECTION("Exception")
+    {
+        CHECK_THROWS(exception<ljh::scope_exit>(count));
+        REQUIRE(count == 1);
+    }
+}
+
+TEST_CASE("scope_fail", "[test_11][defer]")
+{
+    int count = 0;
+    CHECK(count == 0);
+    SECTION("No Exception")
+    {
+        CHECK(no_exception<ljh::scope_fail>(count) == 1);
+        REQUIRE(count == 0);
+    }
+    SECTION("Exception")
+    {
+        CHECK_THROWS(exception<ljh::scope_fail>(count));
+        REQUIRE(count == 1);
+    }
+}
+
+TEST_CASE("scope_success", "[test_11][defer]")
+{
+    int count = 0;
+    CHECK(count == 0);
+    SECTION("No Exception")
+    {
+        CHECK(no_exception<ljh::scope_success>(count) == 1);
+        REQUIRE(count == 1);
+    }
+    SECTION("Exception")
+    {
+        CHECK_THROWS(exception<ljh::scope_success>(count));
+        REQUIRE(count == 0);
+    }
+}
+
+TEST_CASE("condition_scope_exit", "[test_11][defer]")
+{
+    int count = 0;
+    CHECK(count == 0);
+    SECTION("Enabled By Default")
+    {
+        SECTION("Default")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) {};
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_exit>(true, count, action) == 1);
+                REQUIRE(count == 1);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_exit>(true, count, action));
+                REQUIRE(count == 1);
+            }
+        }
+        SECTION("State Changed")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) { d.make_inactive(); };
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_exit>(true, count, action) == 1);
+                REQUIRE(count == 0);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_exit>(true, count, action));
+                REQUIRE(count == 0);
+            }
+        }
+    }
+    SECTION("Disabled By Default")
+    {
+        SECTION("Default")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) {};
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_exit>(false, count, action) == 1);
+                REQUIRE(count == 0);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_exit>(false, count, action));
+                REQUIRE(count == 0);
+            }
+        }
+        SECTION("State Changed")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) { d.make_active(); };
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_exit>(false, count, action) == 1);
+                REQUIRE(count == 1);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_exit>(false, count, action));
+                REQUIRE(count == 1);
+            }
+        }
+    }
+}
+
+TEST_CASE("condition_scope_fail", "[test_11][defer]")
+{
+    int count = 0;
+    CHECK(count == 0);
+    SECTION("Enabled By Default")
+    {
+        SECTION("Default")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) {};
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_fail>(true, count, action) == 1);
+                REQUIRE(count == 0);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_fail>(true, count, action));
+                REQUIRE(count == 1);
+            }
+        }
+        SECTION("State Changed")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) { d.make_inactive(); };
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_fail>(true, count, action) == 1);
+                REQUIRE(count == 0);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_fail>(true, count, action));
+                REQUIRE(count == 0);
+            }
+        }
+    }
+    SECTION("Disabled By Default")
+    {
+        SECTION("Default")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) {};
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_fail>(false, count, action) == 1);
+                REQUIRE(count == 0);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_fail>(false, count, action));
+                REQUIRE(count == 0);
+            }
+        }
+        SECTION("State Changed")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) { d.make_active(); };
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_fail>(false, count, action) == 1);
+                REQUIRE(count == 0);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_fail>(false, count, action));
+                REQUIRE(count == 1);
+            }
+        }
+    }
+}
+
+
+TEST_CASE("condition_scope_success", "[test_11][defer]")
+{
+    int count = 0;
+    CHECK(count == 0);
+    SECTION("Enabled By Default")
+    {
+        SECTION("Default")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) {};
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_success>(true, count, action) == 1);
+                REQUIRE(count == 1);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_success>(true, count, action));
+                REQUIRE(count == 0);
+            }
+        }
+        SECTION("State Changed")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) { d.make_inactive(); };
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_success>(true, count, action) == 1);
+                REQUIRE(count == 0);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_success>(true, count, action));
+                REQUIRE(count == 0);
+            }
+        }
+    }
+    SECTION("Disabled By Default")
+    {
+        SECTION("Default")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) {};
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_success>(false, count, action) == 1);
+                REQUIRE(count == 0);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_success>(false, count, action));
+                REQUIRE(count == 0);
+            }
+        }
+        SECTION("State Changed")
+        {
+            LJH_CPP17_CONSTEXPR auto action = [](auto& d) { d.make_active(); };
+            SECTION("No Exception")
+            {
+                CHECK(no_exception_action<ljh::condition_scope_success>(false, count, action) == 1);
+                REQUIRE(count == 1);
+            }
+            SECTION("Exception")
+            {
+                CHECK_THROWS(exception_action<ljh::condition_scope_success>(false, count, action));
+                REQUIRE(count == 0);
+            }
+        }
+    }
+}
