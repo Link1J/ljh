5333531dcc9a4cf77c437b21396a948ca9a0847a
Link1J <jrairwin@sympatico.ca>
2024-12-28 14:57:39 -0500

    Fix testing, and Cross Compiling.

--- tests/cmake.__cpp_explicit_this_parameter.cpp
+++ tests/cmake.__cpp_explicit_this_parameter.cpp
@@ -1,1 +1,10 @@
+#include <version>
+
+#if _MSC_VER >= 1932 && !defined(__cpp_explicit_this_parameter)
+#define __cpp_explicit_this_parameter 202110L
+#endif
+
 static_assert(__cpp_explicit_this_parameter >= 202110L);
+
+int main()
+{}
